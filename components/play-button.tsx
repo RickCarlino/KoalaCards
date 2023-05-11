8fb1ef66c5701467c34d7e3b531fb88e53bb8ab0
Rick Carlino <rick.carlino@gmail.com>
2023-05-10 21:22:23 -0500

    :heavy_checkmark: Auto play works on Firefox mobile.

--- components/play-button.tsx
+++ components/play-button.tsx
@@ -2,16 +2,59 @@ import { Button } from "@mantine/core";
 import { useHotkeys } from "@mantine/hooks";
 import { useEffect } from "react";
 
-export const playAudio = (dataURI: string): Promise<void> => {
+let audioContext: AudioContext;
+
+// Helper function to play audio from an AudioBuffer
+const playAudioBuffer = (buffer: AudioBuffer): Promise<void> => {
+  return new Promise((resolve) => {
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.onended = () => resolve();
+    source.start(0);
+  });
+};
+
+export const playAudio = (urlOrDataURI: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    if (!dataURI) {
+    if (!urlOrDataURI) {
       resolve();
     }
 
-    const audio = new Audio(dataURI);
-    audio.onended = () => resolve();
-    audio.onerror = (e) => reject(e);
-    audio.play();
+    // Create an AudioContext if it doesn't exist yet
+    if (!audioContext) {
+      audioContext = new AudioContext();
+    }
+
+    // Check if the input is a data URI or a URL
+    if (urlOrDataURI.startsWith("data:")) {
+      // It's a data URI
+
+      // Decode the audio data from the data URI
+      const audioData = atob(urlOrDataURI.split(",")[1]);
+      const audioArray = new Uint8Array(audioData.length);
+      for (let i = 0; i < audioData.length; i++) {
+        audioArray[i] = audioData.charCodeAt(i);
+      }
+
+      // Decode the audio data and play it
+      audioContext.decodeAudioData(
+        audioArray.buffer,
+        (buffer) => {
+          playAudioBuffer(buffer).then(resolve);
+        },
+        (e) => reject(e)
+      );
+    } else {
+      // It's a URL
+
+      // Fetch the audio data from the URL
+      fetch(urlOrDataURI)
+        .then((response) => response.arrayBuffer())
+        .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+        .then((audioBuffer) => playAudioBuffer(audioBuffer).then(resolve))
+        .catch((e) => reject(e));
+    }
   });
 };
 
