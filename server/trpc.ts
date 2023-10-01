c9f1e475a5fd3e9143f60d4b5767667216243c83
Rick Carlino <rick.carlino@gmail.com>
2023-09-30 20:39:14 -0500

    Allowlist by email for private servers.

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,6 +1,12 @@
 import { TRPCError, initTRPC } from "@trpc/server";
 import { Session } from "next-auth";
 import { User } from "@prisma/client";
+const authorizedUsersString = process.env.AUTHORIZED_EMAILS || "";
+const authorizedUsers = authorizedUsersString
+  .split(",")
+  .map((x: string) => x.trim().toLowerCase())
+  .filter((x: string) => x.includes("@"));
+
 const t = initTRPC
   .context<{
     session: Session | null;
@@ -15,6 +21,16 @@ export const procedure = t.procedure.use(
         code: "UNAUTHORIZED",
         message: "Please log in.",
       });
+    }
+    const email = ctx.user?.email || "";
+    if (authorizedUsers.length) {
+      if (!authorizedUsers.includes(email.toLowerCase())) {
+        console.error(`=== Refusing to serve ${email} ===`);
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "You are not authorized to use this service.",
+        });
+      }
     }
     return next();
   }),
