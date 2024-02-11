f687433a9a15ce513400f55e8667a8112244f85e
Rick Carlino <rick.carlino@gmail.com>
2024-02-11 14:39:07 -0600

    Extra hotfixes

--- server/routers/bulk-create-cards.ts
+++ server/routers/bulk-create-cards.ts
@@ -2,6 +2,32 @@ import { z } from "zod";
 import { prismaClient } from "../prisma-client";
 import { procedure } from "../trpc";
 
+function guessLanguage(term: string, definition: string) {
+  const output = { korean: "", english: "" };
+
+  // Regex pattern to match Hangul characters
+  const hangulPattern =
+    /[\uAC00-\uD7A3\u1100-\u11FF\uA960-\uA97F\uD7B0-\uD7FF\u1160-\u11A7\uD7B0-\uD7C6]+/g;
+
+  // Counting Hangul characters in the term and definition
+  const termHangulCount = (term.match(hangulPattern) || []).join("").length;
+  const definitionHangulCount = (definition.match(hangulPattern) || []).join(
+    "",
+  ).length;
+
+  // Determining which one is Korean based on the count of Hangul characters
+  if (termHangulCount > definitionHangulCount) {
+    output.korean = term;
+    output.english = definition;
+  } else {
+    output.korean = definition;
+    output.english = term;
+  }
+
+  console.log(output);
+  return output;
+}
+
 export const bulkCreateCards = procedure
   .input(
     z.object({
@@ -25,7 +51,8 @@ export const bulkCreateCards = procedure
   )
   .mutation(async ({ input, ctx }) => {
     const results: { term: string; definition: string }[] = [];
-    for (const { term: korean, definition: english } of input.input) {
+    for (const { term, definition } of input.input) {
+      const { korean, english } = guessLanguage(term, definition);
       const userId = ctx.user?.id;
       if (userId) {
         const alreadyExists = await prismaClient.card.findFirst({
