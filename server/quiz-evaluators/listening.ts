385d51c70e62f7c1590058c4f3b3251bef0a9f62
Rick Carlino <rick.carlino@gmail.com>
2024-02-22 16:50:34 -0600

    Update listening prompt.

--- server/quiz-evaluators/listening.ts
+++ server/quiz-evaluators/listening.ts
@@ -3,17 +3,20 @@ import { QuizEvaluator } from "./types";
 import { yesOrNo } from "@/utils/openai";
 
 const PROMPT = `
-ISO 639-1:2002 language code: '{{langCode}}'.
-Is the phrase above a correct translation of the phrase "{{term}}"?
-If the answer is "no", provide a reason why. You will be penalized
-for vague responses.`;
+Sentence B: "{{term}}" ({{langCode}})
+Sentence C: "{{definition}}" (EN)
+
+When translated, is sentence A MOSTLY equivalent to sentence B and C?
+The meaning is more important than the words used.
+If "NO", why not?
+`;
 export const listening: QuizEvaluator = async (ctx) => {
   const { userInput, card } = ctx;
   const { term, definition, langCode } = card;
   const tplData = { term, definition, langCode };
   const question = template(PROMPT, tplData);
   const listeningYN = await yesOrNo({
-    userInput,
+    userInput: `Sentence A: ${userInput} (EN)`,
     question,
     userID: ctx.userID,
   });
