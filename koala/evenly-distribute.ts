396743c4c3b2928f4d7dfa40318d6f41020141a7
Rick Carlino <rick.carlino@gmail.com>
2024-06-15 12:33:19 -0500

    Add levelReviews helpers.

--- koala/evenly-distribute.ts
+++ koala/evenly-distribute.ts
@@ -1,3 +1,5 @@
+import { prismaClient } from "./prisma-client";
+
 export function evenlyDistribute<
   T extends { [P in K]: number },
   K extends keyof T,
@@ -18,3 +20,45 @@ export function evenlyDistribute<
   }
   return copy;
 }
+
+export const eligibleForLeveling = async (userID: string) => {
+  const ONE_WEEK = 7 * 24 * 60 * 60 * 1000;
+  const all = await prismaClient.quiz.findMany({
+    where: {
+      Card: {
+        userId: userID,
+        flagged: { not: true },
+      },
+      firstReview: {
+        gt: Date.now() - ONE_WEEK,
+      },
+      nextReview: {
+        lt: Date.now() + ONE_WEEK,
+      },
+      repetitions: {
+        gte: 2,
+      },
+    },
+    orderBy: [{ nextReview: "desc" }],
+    take: 1000,
+  });
+
+  return all.filter((quiz) => {
+    return quiz.repetitions - quiz.lapses > 2;
+  });
+};
+
+export const levelReviews = async (userID: string) => {
+  const eligible = (await eligibleForLeveling(userID)).map((quiz) => ({
+    id: quiz.id,
+    nextReview: quiz.nextReview,
+  }));
+  const evenlyDistributed = evenlyDistribute(eligible, "nextReview");
+  const updates = evenlyDistributed.map(({ id, nextReview }) => {
+    return prismaClient.quiz.update({
+      where: { id },
+      data: { nextReview },
+    });
+  });
+  await prismaClient.$transaction(updates);
+};
