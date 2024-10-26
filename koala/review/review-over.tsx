5df8988599ddaa4b5d49a424825c847718f43e57
Rick Carlino <rick.carlino@gmail.com>
2024-10-25 22:09:57 -0500

    feat: Add review-over component for koala review feature

---
+++ koala/review/review-over.tsx
@@ -0,0 +1,9 @@
+import { QuizState } from "./types";
+
+type ReviewOverProps = {
+  state: QuizState;
+};
+
+export const ReviewOver = (props: ReviewOverProps) => {
+  return <p>Save me, GPT!</p>;
+};
