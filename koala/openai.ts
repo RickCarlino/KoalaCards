f79a74a7df4e54d762e958dff9fb08a46a19889d
Rick Carlino <rick.carlino@gmail.com>
2024-11-27 08:03:37 -0600

    Refactor prompt

--- koala/openai.ts
+++ koala/openai.ts
@@ -16,23 +16,19 @@ export async function gptCall(opts: ChatCompletionCreateParamsNonStreaming) {
   return await openai.chat.completions.create(opts);
 }
 
-const SENTENCE = [
-  `You are a language learning flash card app.`,
-  `You are creating a comic to help users remember the flashcard above.`,
-  `It is a fun, single-frame, black and white comic that illustrates the sentence.`,
-  `Create a DALL-e prompt to create this comic for the card above.`,
-  `Do not add speech bubbles or text. It will give away the answer!`,
-  `All characters must be Koalas.`,
-].join("\n");
+const SENTENCE = `You are a language learning flash card app.
+You are creating a comic to help users remember the flashcard above.
+It is a fun, single-frame, black and white comic that illustrates the sentence.
+Create a DALL-e prompt to create this comic for the card above.
+Do not add speech bubbles or text. It will give away the answer!
+All characters must be Koalas.`;
 
-const SINGLE_WORD = [
-  `You are a language learning flash card app.`,
-  `Create a DALL-e prompt to generate an image of the foreign language word above.`,
-  `Make it as realistic and accurate to the words meaning as possible.`,
-  `The illustration must convey the word's meaning to the student.`,
-  `humans must be shown as anthropomorphized animals.`,
-  `Do not add text. It will give away the answer!`,
-].join("\n");
+const SINGLE_WORD = `You are a language learning flash card app.
+Create a DALL-e prompt to generate an image of the foreign language word above.
+Make it as realistic and accurate to the words meaning as possible.
+The illustration must convey the word's meaning to the student.
+humans must be shown as anthropomorphized animals.
+Do not add text. It will give away the answer!`;
 
 export const createDallEPrompt = async (term: string, definition: string) => {
   const shortCard = term.split(" ").length < 2;
