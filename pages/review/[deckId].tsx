b4845e84b118d9f3494b9996e78f45ffeff5ce1b
Rick Carlino <rick.carlino@gmail.com>
2025-01-04 17:36:14 -0600

    Prevent accidental button clicks, increase lesson size to 24.

--- pages/review/[deckId].tsx
+++ pages/review/[deckId].tsx
@@ -31,7 +31,7 @@ export default function Review() {
     setIsFetching(true);
     // Get the "take" param from the URL using NextJS router.
     const take = parseInt(
-      new URLSearchParams(window.location.search).get("take") || "15",
+      new URLSearchParams(window.location.search).get("take") || "24",
     );
     mutation
       .mutateAsync({ take, deckId }, { onSuccess: (data) => setData(data) })
@@ -62,7 +62,7 @@ export default function Review() {
       />
     );
   } else {
-    el = <div>No quizzes found</div>;
+    el = <div>No quizzes found. Add more words to this deck or come back later when cards are due for review.</div>;
   }
 
   return MicrophonePermissions(el);
