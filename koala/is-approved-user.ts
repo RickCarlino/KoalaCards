9d83f1781cdd66ec45cf3d2c05fb82be56bb4387
Rick Carlino <rick.carlino@gmail.com>
2025-01-05 08:06:54 -0600

    Delete inactive accounts again.

--- koala/is-approved-user.ts
+++ koala/is-approved-user.ts
@@ -1,7 +1,5 @@
 import { prismaClient } from "@/koala/prisma-client";
-// import { timeUntil } from "./time-until";
 
-// Users that are allowed to use GPT-4, etc..
 const superUsers = (process.env.AUTHORIZED_EMAILS || "")
   .split(",")
   .filter((x: string) => x.includes("@"))
@@ -15,34 +13,66 @@ export const isApprovedUser = (id: string) => {
   return approvedUserIDs.has(id) || !env.includes("prod");
 };
 
+function deleteInactiveUser(id: string, email: string | null) {
+  if (isApprovedUser(id)) {
+    return;
+  }
+  prismaClient.user.delete({ where: { id } }).then(() => {
+    console.log(`=== Delete ${email}`);
+  });
+}
+
+function calculateDays(lastSeen: Date | null) {
+  const lastSeenDays = lastSeen
+    ? Math.floor((Date.now() - lastSeen.getTime()) / (1000 * 60 * 60 * 24))
+    : 0;
+
+  return lastSeenDays;
+}
+
+function userApproval(id: string, email: string | null) {
+  const su = email && superUsers.includes(email);
+  if (su) {
+    approvedUserIDs.add(id);
+    return true;
+  }
+  return false;
+}
+
+async function countCards(userId: string) {
+  return prismaClient.card.count({
+    where: {
+      userId,
+    },
+  });
+}
+
+type User = {
+  id: string;
+  email: string | null;
+  lastSeen: Date | null;
+};
+
+const userCleanup = async (user: User) => {
+  const { email, id, lastSeen } = user;
+  const lastSeenDays = calculateDays(lastSeen);
+  if (lastSeenDays < 60) {
+    const su = userApproval(id, email);
+    const cardCount = await countCards(id);
+    if (cardCount > 1) {
+      console.log(`== ${su ? "@" : ""}${email} ${lastSeenDays} days`);
+    } else {
+      // Users must create a card within 5 days of signing up.
+      if (cardCount === 0 && lastSeenDays > 5) {
+        deleteInactiveUser(id, email);
+      }
+    }
+  } else {
+    deleteInactiveUser(user.id, email);
+  }
+};
+
 (async () =>
   prismaClient.user.findMany({}).then(async (users) => {
-    users.map(async (user) => {
-      const { email, /*id,*/ lastSeen } = user;
-      const su = email && superUsers.includes(email);
-      if (su) {
-        approvedUserIDs.add(user.id);
-      }
-      const lastSeenDays = lastSeen
-        ? Math.floor((Date.now() - lastSeen.getTime()) / (1000 * 60 * 60 * 24))
-        : 0;
-      if (lastSeenDays < 93) {
-        const cardCount = await prismaClient.card.count({
-          where: {
-            userId: user.id,
-          },
-        });
-        if (cardCount > 1) {
-          console.log(
-            `=== ${
-              su ? "Super" : "Normal"
-            } user: ${email} (${lastSeenDays} days ago)`,
-          );
-        }
-      } else {
-        // prismaClient.user.delete({ where: { id } }).then(() => {
-        //   console.log(`=== Deleted user: ${email}`);
-        // });
-      }
-    });
+    users.map(userCleanup);
   }))();
