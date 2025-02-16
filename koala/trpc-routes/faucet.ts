c19dfca4429abb388363a2fbe0b0af4fff522b35
Rick Carlino <rick.carlino@gmail.com>
2025-02-16 10:08:56 -0600

    Part of speech tagger idea.

--- koala/trpc-routes/faucet.ts
+++ koala/trpc-routes/faucet.ts
@@ -1,13 +1,83 @@
+import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
-import { procedure } from "../trpc-procedure";
 import { getUserSettings } from "../auth-helpers";
+import { procedure } from "../trpc-procedure";
+import { openai } from "../openai";
+
+const SpeechTageSchema = z.object({
+  words: z.array(
+    z.object({
+      word: z.string(),
+      partOfSpeech: z.union([
+        z.literal("physical-object"),
+        z.literal("noun-not-object"),
+        z.literal("verb"),
+        z.literal("adjective"),
+        z.literal("other"),
+      ]),
+    }),
+  ),
+});
+
+export async function labelWords(
+  // Comma seperated word list.
+  words: string,
+) {
+  const PROMPT = `
+  You are a specialized AI part-of-speech tagger.
+  Your job is to label words with their part of speech
+  for use in a language learning app.
+
+  The labels are: "physical-object", "noun-not-object", "verb", "adjective", "other"
+
+  Pay special attention to the distinction between "noun-not-object" and "physical-object".
+  "noun-not-object" is for non-physical nouns (love, innovation, etc..)
+  "physical-object" can only be used for nouns that are touchable (mountain, shovel, dog, etc..)
+  `;
+  const response = await openai.beta.chat.completions.parse({
+    messages: [
+      { role: "system", content: PROMPT },
+      { role: "user" as const, content: words },
+    ],
+    model: "gpt-4o",
+    temperature: 0.1,
+    response_format: zodResponseFormat(SpeechTageSchema, "parts-of-speech"),
+  });
+
+  // Extract the parsed response from the API's output
+  const parsedResponse = response.choices[0]?.message?.parsed;
+  if (!parsedResponse) {
+    throw new Error("Invalid response format from OpenAI.");
+  }
+
+  const results: Record<"objects" | "words" | "skip", string[]> = {
+    objects: [],
+    words: [],
+    skip: [],
+  };
+  parsedResponse.words.forEach((word) => {
+    switch (word.partOfSpeech) {
+      case "physical-object":
+        results.objects.push(word.word);
+        break;
+      case "noun-not-object":
+      case "verb":
+      case "adjective":
+        results.words.push(word.word);
+        break;
+      default:
+        results.skip.push(word.word);
+    }
+  });
+  return results;
+}
 
 /** The `faucet` route is a mutation that returns a "Hello, world" string
  * and takes an empty object as its only argument. */
 export const faucet = procedure
   .input(
     z.object({
-      cardID: z.string(),
+      words: z.string(),
     }),
   )
   .output(
@@ -18,10 +88,11 @@ export const faucet = procedure
       }),
     ),
   )
-  .mutation(async ({ ctx }) => {
+  .mutation(async ({ ctx, input }) => {
     // I will fill this part out, don't worry about it for now.
     // Show it to you so you understand the schema.
-    const x = await getUserSettings(ctx.user?.id);
-    console.log(x.userId);
+    await getUserSettings(ctx.user?.id);
+    const { words } = await labelWords(input.words);
+    console.log(JSON.stringify(words, null, 2));
     return [];
   });
--- pages/faucet.tsx
+++ pages/faucet.tsx
@@ -1,13 +1,19 @@
-import { Card, Center, Stack } from "@mantine/core";
+import { trpc } from "@/koala/trpc-config";
+import { Button, Card, Center, Stack } from "@mantine/core";
 
 export async function getServerSideProps() {
   return { props: {} };
 }
 
-/** The faucet component has a button that when clicked
- * calls the "faucet" trpc mutation:
- */
+const WORDS = `인마, 펼치다, 높아지다, 상상, 숙소, 거대하다, 승리, 의식, 사모님, 벌어지다`;
+
 export default function Faucet(_props: {}) {
+  const x = trpc.faucet.useMutation();
+  const handler = () => {
+    x.mutateAsync({ words: WORDS }).then(() => {
+      alert("OK");
+    });
+  };
   return (
     <Center style={{ width: "100%" }}>
       <Card
@@ -17,7 +23,9 @@ export default function Faucet(_props: {}) {
         withBorder
         style={{ width: "80%" }}
       >
-        <Stack>TODO</Stack>
+        <Stack>
+          <Button onClick={handler}>TODO</Button>
+        </Stack>
       </Card>
     </Center>
   );
