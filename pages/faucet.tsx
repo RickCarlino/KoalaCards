d75f8b6ddfd0eb1e0d87fb8e31ff9901aa04971f
Rick Carlino <rick.carlino@gmail.com>
2025-02-17 11:25:44 -0600

    Phrase turbine

--- pages/faucet.tsx
+++ pages/faucet.tsx
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import { trpc } from "@/koala/trpc-config";
-import { Button, Card, Center, Stack } from "@mantine/core";
+import { Button, Card, Center, Stack, Textarea } from "@mantine/core";
 
 export async function getServerSideProps() {
   return { props: {} };
 }
 
-const WORDS = `휴대, 자물쇠, 일명, 마주하다, 알아차리다, 시동, 돋보이다, 남매, 도전자, 시위, 뛰어다니다, 음원, 날아다니다, 목격하다, 동선, 두께, 순경, 구조대, 신원, 개인기, 불태우다`;
+export default function Faucet() {
+  const [inputText, setInputText] = useState("");
+  const [output, setOutput] = useState("");
+  const faucetMutation = trpc.faucet.useMutation();
 
-export default function Faucet(_props: {}) {
-  const x = trpc.faucet.useMutation();
-  const handler = () => {
-    x.mutateAsync({ words: WORDS }).then(() => {
-      alert("OK");
-    });
+  const handleSubmit = async () => {
+    try {
+      const result = await faucetMutation.mutateAsync({ words: inputText });
+      // Format the result as pretty JSON
+      setOutput(JSON.stringify(result, null, 2));
+    } catch (error: any) {
+      // Display error as JSON
+      setOutput(JSON.stringify(error, null, 2));
+    }
   };
+
   return (
-    <Center style={{ width: "100%" }}>
+    <Center style={{ width: "100%", padding: "2rem" }}>
       <Card
         shadow="sm"
         padding="lg"
@@ -24,7 +32,23 @@ export default function Faucet(_props: {}) {
         style={{ width: "80%" }}
       >
         <Stack>
-          <Button onClick={handler}>TODO</Button>
+          <Textarea
+            label="Input"
+            placeholder="Enter text here..."
+            value={inputText}
+            onChange={(event) => setInputText(event.currentTarget.value)}
+            autosize
+            minRows={3}
+          />
+          <Button onClick={handleSubmit}>Submit</Button>
+          <Textarea
+            label="Output (JSON)"
+            placeholder="Output will appear here..."
+            value={output}
+            readOnly
+            autosize
+            minRows={3}
+          />
         </Stack>
       </Card>
     </Center>
