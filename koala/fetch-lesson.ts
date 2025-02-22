cf7ecec827a8c4b260a2d40375d56796f572b9c9
Rick Carlino <rick.carlino@gmail.com>
2025-02-22 10:02:35 -0600

    Begin distillation of new prompt. Debug why new cards are not coming up.

--- koala/fetch-lesson.ts
+++ koala/fetch-lesson.ts
@@ -212,6 +212,13 @@ export async function getLessons(p: GetLessonInputParams) {
   );
   const importedEarly = await fetchNewCards(userId, deckId, true, take);
   const importedLate = await fetchNewCards(userId, deckId, false, take);
+  console.log({
+    "====": "====",
+    importedEarly: importedEarly.length,
+    importedLate: importedLate.length,
+    newCardCount,
+    take,
+  });
   const newCards = interleave(newCardCount, importedEarly, importedLate);
   const oldCards = interleave(
     take,
