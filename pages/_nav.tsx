44a1cbf70030e0852514226e3d825e4fbb407a28
Rick Carlino <rick.carlino@gmail.com>
2025-02-23 16:48:37 -0600

    Discord link.

--- pages/_nav.tsx
+++ pages/_nav.tsx
@@ -24,6 +24,7 @@ const NavBar = () => {
     { path: "/cards", name: "Cards" },
     { path: "/user", name: "Settings" },
     { path: "https://github.com/RickCarlino/KoalaCards", name: "Help" },
+    { path: "https://discord.gg/jj7wXhQWJe", name: "Discord", rainbow: true },
   ];
 
   return (
@@ -31,11 +32,44 @@ const NavBar = () => {
       <Link href={"/"} role="img" aria-label="koala" style={linkStyle}>
         🐨
       </Link>
-      {links.map((link, index) => (
-        <Link key={index} href={link.path} style={linkStyle}>
-          {link.name}
-        </Link>
-      ))}
+      {links.map((link, index) => {
+        if (link.rainbow) {
+          const rainbowStyle = {
+            ...linkStyle,
+            animation: "rainbow 1s linear 1",
+          };
+
+          // Adding the keyframes to the style object
+          // Lets update this to make it shake
+          const keyframes = `
+            @keyframes rainbow {
+              0% { color: red; rotate: 0deg; }
+              14% { color: orange; rotate: 10deg; }
+              28% { color: yellow; rotate: -10deg; }
+              42% { color: green; rotate: 10deg; }
+              57% { color: blue; rotate: -10deg; }
+              71% { color: indigo; rotate: 10deg; }
+              85% { color: violet; rotate: -10deg; }
+              100% { color: red; rotate: 0deg; }
+            }
+          `;
+
+          return (
+            <>
+              <style>{keyframes}</style>{" "}
+              {/* Insert the keyframes into the document */}
+              <Link key={index} href={link.path} style={rainbowStyle}>
+                {link.name}
+              </Link>
+            </>
+          );
+        }
+        return (
+          <Link key={index} href={link.path} style={linkStyle}>
+            {link.name}
+          </Link>
+        );
+      })}
     </div>
   );
 };
