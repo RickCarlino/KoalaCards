5ca7784821c76858794e27a0d77bbe86f0bc1aa1
Rick Carlino <rick.carlino@gmail.com>
2025-05-25 07:51:43 -0500

    Update defaults.

---
+++ prisma/migrations/20250525124758_change_defaults/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "UserSettings" ALTER COLUMN "cardsPerDayMax" SET DEFAULT 7,
+ALTER COLUMN "playbackPercentage" SET DEFAULT 0.125,
+ALTER COLUMN "dailyWritingGoal" SET DEFAULT 150;
