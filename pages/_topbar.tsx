c80f29a905782cbb3f529064b38f9685c32c6603
Rick Carlino <rick.carlino@gmail.com>
2025-06-02 01:35:28 -0500

    Logo broke...?

--- pages/_topbar.tsx
+++ pages/_topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { AppShell, Group, useMantineTheme } from "@mantine/core";
+import { IconArrowBack } from "@tabler/icons-react";
 
 interface TopBarProps {
   children: React.ReactNode;
@@ -9,7 +10,23 @@ interface TopBarProps {
 
 const TopBar = ({ children }: TopBarProps) => {
   const theme = useMantineTheme();
-
+  const logo = (
+    <Image
+      src="/nav.png"
+      alt="Koala Cards Logo"
+      width={150}
+      height={50}
+      style={{
+        display: "block",
+        objectFit: "contain",
+        maxHeight: "100%",
+      }}
+    />
+  );
+  const backButton = (
+    <IconArrowBack size={24} color={theme.colors.pink[7]} />
+  );
+  const topThing = window.location.pathname === "/" ? logo : backButton;
   return (
     <AppShell
       header={{ height: { base: 60, md: 70, lg: 80 } }}
@@ -24,17 +41,7 @@ const TopBar = ({ children }: TopBarProps) => {
       <AppShell.Header>
         <Group h="100%" px="md" justify="center">
           <Link href="/" style={{ textDecoration: "none" }}>
-            <Image
-              src="/nav.png"
-              alt="Koala Cards Logo"
-              width={150}
-              height={50}
-              style={{
-                display: "block",
-                objectFit: "contain",
-                maxHeight: "100%",
-              }}
-            />
+            {topThing}
           </Link>
         </Group>
       </AppShell.Header>
